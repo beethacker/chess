b432dc0ffeb8a923be0310550fc6ab50c4ee04fb
BeetHacker <brodiechampion@gmail.com>
2021-05-06 11:51:27 -0300

    Draw board from FEN in url

--- chess.js
+++ chess.js
@@ -1,8 +1,14 @@
-//Auto reload. Ideally we'd only do this when our time stamps change, so we want a 
-//a little utility that updates another file that we read for a timestamp, etc.
-window.setTimeout(function () {
-   // window.location = "index.html?nocache=" + (new Date()).getTime();
-}, 4000);
+window.setTimeout(function () { 
+	// window.location = "index.html?nocache=" + (new Date()).getTime();
+}, 4000); 
+
+$(window).resize(function() {
+	resize();
+});
+
+const CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_";
+//a=97
+//A=65
 
 const KING = 0;
 const QUEEN = 1;
@@ -23,40 +29,168 @@ const PIECE_NAMES = {
 const WHITE = 0;
 const BLACK = 1;
 
-function makePieceImg(piece) {
-	var scale = 1;
-	var top = scale*200*piece.color;
-	var bottom = top + scale*200;
-	var left = scale*200*piece.type;
-	var right = left + scale*200;
-	right = 1200;
-	bottom = 400;
-	var w = 200;
-	var h = 200;
+const SPRITE_DIM = 200;
 
-	return `<div class="piece" style="width:${w}px;height:${h}px;"><img src="sprites.png" class="piece" style="margin-left: ${-left}px; width: ${right}px; margin-top: ${-top}px; height: ${bottom}px;transform:scale(${scale})"></div></div>`;
+var state = {
+	"mouseDown" : false,
+	"pieces" : [],
+	"inHand" : null
+};
+
+function resize() {
+	var ctx = document.querySelector("#canvas").getContext("2d");
+
+	//TODO do this on a tick....
+	var dim = 8*Math.floor(0.7*Math.min(window.innerWidth, window.innerHeight)/8);
+	if (dim != ctx.canvas.width) {
+		ctx.canvas.width = dim;
+		ctx.canvas.height = dim;
+		render(ctx, "");
+	}	
 }
 
-function drawPiece(coord, piece) {
-	$("#" + coord).empty().append(makePieceImg(piece));
+function onMouseDown(event) {
+	// console.log(event);
+	state.mouseDown = true;
 }
 
-function render(state) {
+function onMouseUp(event) {
+	// console.log(event);
+	state.mouseDown = false;
+}
+
+function onMouseMove(event) {
+	if (state.mouseDown) {
+		// console.log("Move: " + event.offsetX + ", " + event.offsetY);
+	}
+}
+
+function drawPiece(ctx, img, size, coord, piece) {
+	var sleft = SPRITE_DIM*piece.type;
+	var stop = SPRITE_DIM*piece.color;
+	ctx.drawImage(img, sleft, stop, SPRITE_DIM, SPRITE_DIM, coord.col*size, coord.row*size, size, size);
+}
+
+function render(ctx) {
 	console.log("Render: " + state);
+	ctx.fillStyle = "#000000";
+	ctx.fillRect(0, 0, 400, 400);
 
-	//NOTE for now, let's just stub in the stuff.
-	drawPiece("03", {color:WHITE, type:ROOK});
-	drawPiece("77", {color:BLACK, type:QUEEN});
-	drawPiece("test", {color:BLACK, type:QUEEN});
-}
+	ctx.fillStyle = "#F0DB4F";
+	ctx.fillRect(50, 50, 200, 200);
 
-$(document).ready(function() {
+
+	var size = Math.floor(ctx.canvas.width / 8);
 	for (var i = 0; i < 64; i++) {
 		var row = Math.floor(i / 8);
 		var col = i % 8;
-		var color = ((row % 2) == (col % 2)) ? "white" : "black";
-		var coord = "" + row + col;
-		$("#board").append(`<div id=${coord} class="square ${color}"/>`);
+		if ((row%2)==(col%2)) {
+			ctx.fillStyle = "#FFDDDD";
+		}
+		else {
+			ctx.fillStyle = "#3333FF";
+		}
+		
+		ctx.fillRect(col*size, row*size, size, size);
 	}
-	render();
+
+
+	var img = document.querySelector("#sprites");
+	//NOTE for now, let's just stub in the stuff.
+	/*
+	drawPiece(ctx, img, size, {row:0, col:2}, {color:WHITE, type:ROOK});
+	drawPiece(ctx, img, size, {row:7, col:7}, {color:BLACK, type:QUEEN});
+	for (var i = 0; i < 7; i++) {
+		drawPiece(ctx, img, size, {row:1, col:i}, {color:WHITE, type:PAWN});
+		drawPiece(ctx, img, size, {row:6, col:i}, {color:BLACK, type:PAWN});
+	}
+	*/
+	for (var i = 0; i < state.pieces.length; i++) {
+		var piece = state.pieces[i];
+		drawPiece(ctx, img, size, piece, piece);
+	}
+}
+
+function fenPiece(char) {
+	var result = {};
+	switch (char) {
+		case 'p': return {color: BLACK, type: PAWN};
+		case 'r': return {color: BLACK, type: ROOK};
+		case 'n': return {color: BLACK, type: KNIGHT};
+		case 'b': return {color: BLACK, type: BISHOP};
+		case 'k': return {color: BLACK, type: KING};
+		case 'q': return {color: BLACK, type: QUEEN};
+		case 'P': return {color: WHITE, type: PAWN};
+		case 'R': return {color: WHITE, type: ROOK};
+		case 'B': return {color: WHITE, type: BISHOP};
+		case 'N': return {color: WHITE, type: KNIGHT};
+		case 'K': return {color: WHITE, type: KING};
+		case 'Q': return {color: WHITE, type: QUEEN};
+	}
+
+	var code = char.charCodeAt(0);
+	if (code == 48) {
+		return {space: 10};
+	}
+	else if (code > 48 && code < 58) {
+		return {space: code - 48};
+	}
+
+	return {};
+}
+
+$(document).ready(function() {
+	$("#canvas").mousedown(onMouseDown).mouseup(onMouseUp).mousemove(onMouseMove);
+
+	var sPageURL = window.location.search.substring(1);
+	console.log(sPageURL);
+	var sURLVar = sPageURL.split("&");
+	var parameters = {};
+	for (var i = 0; i < sURLVar.length; i++) {
+		var nameVal = sURLVar[i].split("=");
+		parameters[nameVal[0]] = nameVal[1];
+	}
+	console.log(parameters);
+
+
+
+	//TODO we'll grab this from url in a minute, but for now, lets hardcode in
+	//a list of 64 squares, each showing whats on it. we can use FEN notation here
+	//that gives a pretty compressed sense of it. ignore _.
+	// board, turn, castle availability, en passant availability, turn num.
+
+
+	// console.log(fen);
+	/*
+	console.log("a =>" + "a".charCodeAt(0));
+	console.log("z =>" + "z".charCodeAt(0));
+	console.log("A =>" + "A".charCodeAt(0));
+	console.log("Z =>" + "Z".charCodeAt(0));
+	console.log("0 =>" + "0".charCodeAt(0));
+	*/
+	var fen = parameters.fen;
+	if (fen == null) {
+		fen = "rnbqkbnr_pppppppp_8_8_8_8_PPPPPPPP_RNBQKBNR";
+	}
+	fen = fen.replaceAll("_", "");
+
+	var square = 0;
+	for (var i = 0; i < fen.length; i++) {
+		var piece = fenPiece(fen.charAt(i));	
+		if ("color" in piece) {
+			piece.row = Math.floor(square/8);
+			piece.col = square%8;
+			state.pieces.push(piece);
+			square++;
+		}
+		else if ("space" in piece) {
+			square += piece.space; 
+		}
+		else {
+			alert("can't parse: " + fen[i]);
+		}
+	}
+	console.log(state.pieces);
+
+	resize();
 });
